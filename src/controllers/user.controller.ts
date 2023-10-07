443d0856b4c2768b9db26e17b937757dd3414596
Camila Martinez <camilamarb98@gmail.com>
2023-10-07 23:49:02 +0200

    Add types

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const users = await prisma.user.findMany()
+    const users: User[] = await prisma.user.findMany()
     res.status(200).json({
       users,
     })
