c910099236fb36c661c6fa7117ba8f4acbbdf994
Camila Martinez <camilamarb98@gmail.com>
2023-10-08 12:22:00 +0200

    Update User controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import { User } from '@prisma/client'
 import bcrypt from 'bcrypt'
 
 export const newUser = async (req: Request, res: Response) => {
-  const { username, email, firstName, lastName, password, role } = req.body
+  const { username, email, firstName, lastName, password, roleId } = req.body
   if (!username || !password)
     return res
       .status(400)
@@ -15,17 +15,17 @@ export const newUser = async (req: Request, res: Response) => {
       OR: [{ username: username }, { email: email }],
     },
   })
-  if (user) return res.sendStatus(409) // Conflict
+  if (user.length > 0) return res.sendStatus(409) // Conflict
   try {
     const hashedPassword = await bcrypt.hash(password, 10)
-    const user = await prisma.user.create({
+    const user: User = await prisma.user.create({
       data: {
         firstName: firstName,
         lastName: lastName,
         username: username,
         email: email,
         password: hashedPassword,
-        roleId: role,
+        roleId: roleId,
       },
     })
     res.status(201).json({ success: 'User created successfully' })
