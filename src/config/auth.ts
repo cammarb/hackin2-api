183537fae069288cb8ca5d344311b439d7cc8a48
Camila Martinez <camilamarb98@gmail.com>
2023-10-15 13:04:53 +0200

    Update auth config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -6,29 +6,28 @@ const generateAccessToken = (user: User): string => {
   const options: SignOptions = { expiresIn: '5m' }
   if (!process.env.JWT_ACCESS_SECRET)
     throw new Error('JWT_REFRESH_SECRET is not defined')
-
   const accessSecret: jwt.Secret = process.env.JWT_ACCESS_SECRET
+
   return jwt.sign(payload, accessSecret, options)
 }
 
-const generateRefreshToken = (user: User, jti: string): string => {
-  const payload: JwtPayload = { userId: user.id, jti }
+const generateRefreshToken = (user: User): string => {
+  const payload: JwtPayload = { userId: user.id }
   const options: SignOptions = {
     expiresIn: '8h',
   }
   if (!process.env.JWT_REFRESH_SECRET)
     throw new Error('JWT_REFRESH_SECRET is not defined')
-
   const refreshToken: jwt.Secret = process.env.JWT_REFRESH_SECRET
+
   return jwt.sign(payload, refreshToken, options)
 }
 
 const generateTokens = (
-  user: User,
-  jti: string
+  user: User
 ): { accessToken: string; refreshToken: string } => {
   const accessToken = generateAccessToken(user)
-  const refreshToken = generateRefreshToken(user, jti)
+  const refreshToken = generateRefreshToken(user)
 
   return {
     accessToken,
