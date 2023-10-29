3b2ae6f1dae11b20fb652c697a3234d2926fc82c
Camila Martinez <camilamarb98@gmail.com>
2023-10-29 12:27:30 +0100

    Update refreshToken controller

--- src/controllers/refreshToken.controller.ts
+++ src/controllers/refreshToken.controller.ts
@@ -1,49 +1,35 @@
 import { Request, Response } from 'express'
-import bcrypt from 'bcrypt'
 import prisma from '../config/db'
 import { RefreshToken, User } from '@prisma/client'
 import { generateTokens } from '../config/auth'
+import { verifyJWT } from '../middleware/verifyJWT'
 
-const refreshToken = async (req: Request, res: Response) => {
-  const { username, password } = req.body
-  if (!username || !password)
-    res.status(400).json({
-      error: 'Bad Request',
-      message: 'Both username and password are required',
-    })
-  const user: User | null = await prisma.user.findUnique({
+const handleRefreshToken = async (req: Request, res: Response) => {
+  const header = req.headers
+  if (!header.authorization) return res.status(401)
+  const refreshToken = header.authorization
+
+  const user: User | null = await prisma.user.findFirst({
     where: {
-      username: username,
+      RefreshToken: {
+        some: {
+          hashedToken: refreshToken,
+        },
+      },
     },
   })
-  if (!user) {
-    res.status(401).json({
-      error: 'Authentication failed',
-      message: 'Invalid username or password',
-    })
-  } else {
-    const passwordMatch = await bcrypt.compare(password, user.password)
-    if (passwordMatch) {
-      const tokens = generateTokens(user)
-      const refreshToken: RefreshToken = await prisma.refreshToken.create({
-        data: {
-          hashedToken: tokens.refreshToken,
-          userId: user.id,
-        },
-      })
-      res.header('Authorization', `Bearer ${tokens.accessToken}`)
-      res.header('Refresh-Token', tokens.refreshToken)
+  if (!user) return res.sendStatus(403) // Forbidden
 
-      res.status(200).json({
-        message: 'Login successful',
-      })
-    } else {
-      res.status(401).json({
-        error: 'Authentication failed',
-        message: 'Invalid username or password',
-      })
-    }
-  }
+  verifyJWT(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username)
+      return res.sendStatus(403)
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '30s' }
+    )
+    res.json({ accessToken })
+  })
 }
 
-export { refreshToken }
+export { handleRefreshToken }
