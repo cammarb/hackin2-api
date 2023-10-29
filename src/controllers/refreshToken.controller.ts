8a83a76f19a20397e4798dcad0910e97e2dd5a31
Camila Martinez <camilamarb98@gmail.com>
2023-10-29 11:51:54 +0100

    Add refreshToken template

---
+++ src/controllers/refreshToken.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
+import prisma from '../config/db'
+import { RefreshToken, User } from '@prisma/client'
+import { generateTokens } from '../config/auth'
+
+const refreshToken = async (req: Request, res: Response) => {
+  const { username, password } = req.body
+  if (!username || !password)
+    res.status(400).json({
+      error: 'Bad Request',
+      message: 'Both username and password are required',
+    })
+  const user: User | null = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  })
+  if (!user) {
+    res.status(401).json({
+      error: 'Authentication failed',
+      message: 'Invalid username or password',
+    })
+  } else {
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (passwordMatch) {
+      const tokens = generateTokens(user)
+      const refreshToken: RefreshToken = await prisma.refreshToken.create({
+        data: {
+          hashedToken: tokens.refreshToken,
+          userId: user.id,
+        },
+      })
+      res.header('Authorization', `Bearer ${tokens.accessToken}`)
+      res.header('Refresh-Token', tokens.refreshToken)
+
+      res.status(200).json({
+        message: 'Login successful',
+      })
+    } else {
+      res.status(401).json({
+        error: 'Authentication failed',
+        message: 'Invalid username or password',
+      })
+    }
+  }
+}
+
+export { refreshToken }
