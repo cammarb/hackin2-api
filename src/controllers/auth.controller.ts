bbd56855170879a3b4c351e853375ed0c4456fac
Camila Martinez <camilamarb98@gmail.com>
2023-11-01 18:06:42 +0100

    refactor auth controller (tokens)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
 import prisma from '../config/db'
 import { RefreshToken, User } from '@prisma/client'
-import { generateTokens } from '../config/auth'
+import { generateAccessToken, generateTokens } from '../config/auth'
+import jwt from 'jsonwebtoken'
 
 const handleLogin = async (req: Request, res: Response) => {
   const { username, password } = req.body
@@ -32,7 +33,12 @@ const handleLogin = async (req: Request, res: Response) => {
         },
       })
       res.header('Authorization', `Bearer ${tokens.accessToken}`)
-      res.header('Refresh-Token', tokens.refreshToken)
+      res.cookie('Refresh-Token', tokens.refreshToken, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
 
       res.status(200).json({
         message: 'Login successful',
@@ -43,6 +49,39 @@ const handleLogin = async (req: Request, res: Response) => {
         message: 'Invalid username or password',
       })
     }
+  }
+}
+
+const handleRefreshToken = async (req: Request, res: Response) => {
+  const header = req.headers
+  if (!header.authorization) return res.status(401)
+  const refreshToken = header.authorization
+
+  const user: User | null = await prisma.user.findFirst({
+    where: {
+      RefreshToken: {
+        some: {
+          hashedToken: refreshToken,
+        },
+      },
+    },
+  })
+  if (!user) return res.sendStatus(403) // Forbidden
+
+  const tokenSecretKey = process.env.SECRET_KEY
+  const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY
+
+  if (!tokenSecretKey || !refreshTokenSecretKey) {
+    return res.sendStatus(403)
+  } else {
+    jwt.verify(refreshToken, refreshTokenSecretKey, (err, decoded) => {
+      if (err || !decoded || user.username !== decoded.username) {
+        return res.sendStatus(403)
+      }
+
+      const accessToken = generateAccessToken(user)
+      res.json({ accessToken })
+    })
   }
 }
 
