6f925b9524dbdcebe5fa53d2bfcefd358be2e9e8
Camila Martinez <camilamarb98@gmail.com>
2023-11-03 16:22:03 +0100

    cleanup code

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -3,10 +3,12 @@ import jwt, { DecodeOptions } from 'jsonwebtoken'
 
 const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization']
-  if (!authHeader) return res.sendStatus(401)
-  const token = authHeader.split(' ')[1]
 
-  const secret = process.env.JWT_ACCESS_SECRET
+  if (!authHeader) return res.sendStatus(401)
+
+  const token = authHeader.split(' ')[1]
+  const secret = process.env.ACCESS_TOKEN_SECRET
+
   if (secret) {
     try {
       const decoded = jwt.verify(token, secret)
