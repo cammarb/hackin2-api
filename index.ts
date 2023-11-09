82e6e9b19d3d1e364c468950565a64955fb30a10
Camila Martinez <camilamarb98@gmail.com>
2023-11-10 00:28:47 +0100

    add cookie parser

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import path from 'path'
 import cors from 'cors'
 
 import userRouter from './src/routes/user.routes'
 import authRouter from './src/routes/auth.routes'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
 
+app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 
