0c6a5f2d5651b6dcb78a2aefca1c7fa81c37daf2
Camila Martinez <camilamarb98@gmail.com>
2023-11-13 21:38:04 +0100

    Add Jest and Supertest

---
+++ __tests__/routes/user.routes.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import app from '../../src/app'
+
+describe('User routes', () => {
+  test('Get all users', async () => {
+    const res = await request(app).get('/users')
+
+    expect(res.body.users).toEqual(
+      expect.arrayContaining([
+        {
+          id: expect.any(Number),
+          firstName: expect.any(String),
+          lastName: expect.any(String),
+          username: expect.any(String),
+          email: expect.any(String),
+          password: expect.any(String),
+          roleId: expect.any(Number),
+        },
+      ])
+    )
+  })
+})
