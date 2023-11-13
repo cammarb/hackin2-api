d364099813dc0bab6357f3dd5edd7c561031015d
Camila Martinez <camilamarb98@gmail.com>
2023-11-13 20:32:38 +0100

    Update JWT verifier middleware

--- src/middleware/auth.middelware.ts
+++ src/middleware/auth.middelware.ts
@@ -1,21 +1,32 @@
-import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
 import fs from 'fs'
+import jwt from 'jsonwebtoken'
 
-const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization']
 
   if (!authHeader) return res.sendStatus(401)
 
   const token = authHeader.split(' ')[1]
 
-  const secret = fs.readFileSync(`${process.env.PUBKEY}`, 'utf8')
+  try {
+    const secret = await fs.promises.readFile(`${process.env.PUBKEY}`, 'utf8')
 
-  if (!secret) res.status(401).json({ error: 'Verification failed' })
+    if (!secret) {
+      return res.status(401).json({ error: 'Verification failed' })
+    }
 
-  const decoded = jwt.verify(token, secret)
-  if (!decoded) return res.sendStatus(403)
-  next()
+    const decoded = jwt.verify(token, secret) as {
+      username: string
+      role: string
+    }
+
+    req.params.username = decoded.username
+    req.params.roleId = decoded.role
+    next()
+  } catch (error) {
+    return res.sendStatus(403)
+  }
 }
 
 export { verifyJWT }
