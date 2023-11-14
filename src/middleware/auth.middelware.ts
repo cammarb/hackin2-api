e0bbf289145c5425c0a1a610a11ae7d179e726a3
Camila Martinez <camilamarb98@gmail.com>
2023-11-14 18:25:56 +0100

    refactor middleware

--- src/middleware/auth.middelware.ts
+++ src/middleware/auth.middelware.ts
@@ -4,18 +4,13 @@ import jwt from 'jsonwebtoken'
 
 const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization']
+  const secret = await fs.promises.readFile(`${process.env.PUBKEY}`, 'utf8')
 
-  if (!authHeader) return res.sendStatus(401)
+  if (!authHeader || !secret) return res.sendStatus(401)
 
   const token = authHeader.split(' ')[1]
 
   try {
-    const secret = await fs.promises.readFile(`${process.env.PUBKEY}`, 'utf8')
-
-    if (!secret) {
-      return res.status(401).json({ error: 'Verification failed' })
-    }
-
     const decoded = jwt.verify(token, secret) as {
       username: string
       role: string
