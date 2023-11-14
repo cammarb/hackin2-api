7dc5075c0963118694ceff5c8c96d0672811035c
Camila Martinez <camilamarb98@gmail.com>
2023-11-14 15:55:03 +0100

    add auth tests

--- __tests__/config/auth.test.ts
+++ __tests__/config/auth.test.ts
@@ -1,22 +1,56 @@
 import { generateTokens } from '../../src/config/auth'
 import fs from 'fs'
 import { User } from '@prisma/client'
+import { generateKeyPairSync } from 'crypto'
 
 jest.mock('fs')
 
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem',
+  },
+})
+
+process.env.PRIVKEY = privateKey
+process.env.ISSUER = 'exampleIssuer'
+
+const user: User = {
+  id: 1,
+  firstName: 'Test',
+  lastName: 'Testing',
+  username: 'test.testing',
+  email: 'test.testing@example.com',
+  password: 'hashed-password',
+  roleId: 1,
+}
+
+describe('generateAccessToken', () => {
+  test('Should generate the tokens with valid private key', () => {
+    ;(fs.readFileSync as jest.Mock).mockReturnValue(process.env.PRIVKEY)
+
+    const tokens = generateTokens(user)
+
+    const accessToken = tokens.accessToken
+    const refreshToken = tokens.refreshToken
+
+    expect(typeof accessToken).toBe('string')
+    expect(accessToken.length).toBeGreaterThan(0)
+
+    expect(typeof refreshToken).toBe('string')
+    expect(refreshToken.length).toBeGreaterThan(0)
+  })
+})
+
 describe('generateTokens', () => {
   test('Should generate the tokens with valid private key', () => {
-    ;(fs.readFileSync as jest.Mock).mockReturnValue('valid-private-key')
+    ;(fs.readFileSync as jest.Mock).mockReturnValue(process.env.PRIVKEY)
 
-    const user: User = {
-      id: 1,
-      firstName: 'Test',
-      lastName: 'Testing',
-      username: 'test.testing',
-      email: 'test.testing@example.com',
-      password: 'hashed-password',
-      roleId: 1,
-    }
     const tokens = generateTokens(user)
 
     const accessToken = tokens.accessToken
