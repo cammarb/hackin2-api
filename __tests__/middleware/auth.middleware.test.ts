37b2b2e9aa75a64c28c3ccb5ac89072f0990d30c
Camila Martinez <camilamarb98@gmail.com>
2023-11-14 19:10:33 +0100

    update test

--- __tests__/middleware/auth.middleware.test.ts
+++ __tests__/middleware/auth.middleware.test.ts
@@ -1,28 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
-import fs from 'fs/promises'
 import jwt from 'jsonwebtoken'
 import { verifyJWT } from '../../src/middleware/auth.middleware'
-import { generateKeyPairSync } from 'crypto'
-
-const { privateKey, publicKey } = generateKeyPairSync('rsa', {
-  modulusLength: 2048,
-  publicKeyEncoding: {
-    type: 'spki',
-    format: 'pem',
-  },
-  privateKeyEncoding: {
-    type: 'pkcs8',
-    format: 'pem',
-  },
-})
-
-jest.mock('fs/promises')
 
 describe('JWT Middleware Tests', () => {
-  beforeEach(() => {
-    jest.restoreAllMocks() // Ensure mocks are cleared before each test
-  })
-
   it('should verify the token and set params in request', async () => {
     const req: Request = {
       headers: { authorization: 'Bearer valid_token' },
@@ -31,11 +11,11 @@ describe('JWT Middleware Tests', () => {
     const res: Response = { sendStatus: jest.fn() } as unknown as Response
     const next: NextFunction = jest.fn() as NextFunction
 
-    jest
+    const mockedVerify = jest
       .spyOn(jwt, 'verify')
-      .mockImplementation((_token, _secretOrPublicKey, _options, callback) => {
+      .mockImplementationOnce((token, publicKey, options, callback) => {
         if (callback) {
-          return callback(null, { username: 'test', role: '1' })
+          callback(null, { username: 'test', role: '1' })
         } else {
           throw new Error('Callback function is not defined')
         }
@@ -43,10 +23,16 @@ describe('JWT Middleware Tests', () => {
 
     await verifyJWT(req, res, next)
 
-    expect(jwt.verify).toHaveBeenCalledWith()
+    expect(req.headers.authorization).toEqual('Bearer valid_token')
+    expect(mockedVerify).toHaveBeenCalledWith(
+      'valid_token',
+      'your_public_key',
+      undefined,
+      expect.any(Function)
+    )
 
     expect(req.params.username).toEqual('test')
-    expect(req.params.roleId).toEqual('1')
+    expect(req.params.role).toEqual('1')
 
     expect(next).toHaveBeenCalled()
   })
