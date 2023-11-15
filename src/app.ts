63ce1ff056ff5de6cc3f83350b063b16246938da
Camila Martinez <camilamarb98@gmail.com>
2023-11-15 20:54:42 +0100

    Add helmet

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import fs from 'fs'
 import userRouter from './routes/user.routes'
 import authRouter from './routes/auth.routes'
 import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
 
 dotenv.config()
 
@@ -17,15 +18,17 @@ const publicKey = fs.readFileSync(`${process.env.PUBKEY}`, {
   encoding: 'utf-8',
 })
 const issuer = process.env.ISSUER
+const origin = process.env.ORIGIN
 
 app.use(cookieParser())
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: origin,
     methods: ['GET', 'POST', 'PUT'], // Allow only specified HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers)
   })
 )
+app.use(helmet)
 app.use(express.json())
 
 app.use('/user', userRouter)
