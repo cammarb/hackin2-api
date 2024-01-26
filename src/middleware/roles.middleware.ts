e22cacd6f92d4648573c671218a800515cb67cca
Camila Martinez <camilamarb98@gmail.com>
2024-01-26 08:46:27 +0100

    Add check Manager middleware

--- src/middleware/roles.middleware.ts
+++ src/middleware/roles.middleware.ts
@@ -28,9 +28,44 @@ const checkAdmin = async (req: Request, res: Response, next: NextFunction) => {
       return res.status(403).json({ error: 'Permission denied' })
     }
   } catch (error) {
-    console.error('Error in checkAdmin middleware:', error)
+    console.error('Error in role middleware:', error)
     return res.status(500).json({ error: 'Internal server error' })
   }
 }
 
-export { checkAdmin }
+const checkManager = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const username = req.params.username
+    const role = req.params.role
+
+    if (!username) {
+      return res.status(400).json({ error: 'payload not provided' })
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { username: username },
+      select: { role: true },
+    })
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    const userRole = user.role
+
+    if (role === userRole && userRole === 'MANAGER') {
+      return next()
+    } else if (role === userRole && userRole === 'ADMIN') {
+      return next()
+    } else return res.status(403).json({ error: 'Permission denied' })
+  } catch (error) {
+    console.error('Error in role middleware:', error)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
+export { checkAdmin, checkManager }
