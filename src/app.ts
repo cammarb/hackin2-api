c83df2241365d18dc827d0eed06ff461a37bef2e
Camila Martinez <camilamarb98@gmail.com>
2024-01-29 16:08:59 +0000

    Add morgan logger

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import fs from 'fs'
+import morgan from 'morgan'
 
 import userRouter from './routes/user.routes'
 import authRouter from './routes/auth.routes'
@@ -27,14 +28,15 @@ app.use(
     origin: origin,
     methods: ['GET', 'POST', 'PUT'], // Allow only specified HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers)
-  })
+  }),
 )
 app.use(helmet())
 app.use(cookieParser())
 app.use(express.json())
+app.use(morgan('tiny'))
 
-app.use('/', (req, res)=>{
-  res.send("Welcome to the Hackin2 API.")
+app.get('/', (req, res) => {
+  res.send('Welcome to the Hackin2 API.')
 })
 app.use('/user', userRouter)
 app.use('/auth', authRouter)
