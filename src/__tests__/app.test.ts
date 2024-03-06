a9f8981f3f13299e8ddf8d547740fd4733082c21
Camila Martinez <camilamarb98@gmail.com>
2024-03-06 20:31:44 +0100

    Update tests

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,11 +1,13 @@
 import request from 'supertest'
 
-import app from '../src/app'
+import createServer from '../utilts/server'
 
-describe('Test app.ts', () => {
-  test('Main route', async () => {
+const app = createServer()
+
+describe('app.ts', () => {
+  it('should respond with a 200 status in /api/v*/ ', async () => {
     return request(app)
-      .get('/api/v1')
+      .get('/')
       .then((res) => {
         expect(res.statusCode).toBe(200)
         expect(res.body).toEqual({ message: 'Welcome to the Hackin2 API.' })
