6f8c1196e73de012236b55bea9c175c426eee890
Camila Martinez <camilamarb98@gmail.com>
2024-07-03 13:41:07 +0200

    Add session route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,6 +4,7 @@ import {
   handleLogin,
   handleRefreshToken,
   handleRegistration,
+  handleSession,
 } from '../controllers/auth.controller'
 
 const authRouter: Router = Router()
@@ -12,5 +13,6 @@ authRouter.post('/register', handleRegistration)
 authRouter.post('/login', handleLogin)
 authRouter.post('/logout', handleLogOut)
 authRouter.get('/refresh', handleRefreshToken)
+authRouter.get('/session', handleSession)
 
 export default authRouter
