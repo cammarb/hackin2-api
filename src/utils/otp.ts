598c679568492298da7eb258e5eb393a66941a23
Camila Martinez <camilamarb98@gmail.com>
2024-07-04 14:29:01 +0200

    Add OTP Generator at registration: - Add util to generate OTP and send it to the respective email - Add new env variables to handle the sending of the email - Generate OTP and send to email after registration - Add OTP validator route - Add initial tests

---
+++ src/utils/otp.ts
@@ -0,0 +1,30 @@
+import { generate } from 'otp-generator'
+import { createTransport } from 'nodemailer'
+
+export const generateOTP = (): string => {
+  const otp = generate(6, {
+    digits: true,
+    upperCaseAlphabets: true,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  })
+  return otp
+}
+
+export const sendOTPEmail = async (email: string, otp: string) => {
+  const transport = createTransport({
+    service: process.env.OTP_SERVICE,
+    auth: {
+      user: process.env.OTP_USER,
+      pass: process.env.OTP_PASS,
+    },
+  })
+  const info = await transport.sendMail({
+    from: process.env.OTP_USER,
+    to: email,
+    subject: 'Hackin2 - Verification Code',
+    text: `Your OTP code is: ${otp}`,
+  })
+
+  console.log('message sent')
+}
