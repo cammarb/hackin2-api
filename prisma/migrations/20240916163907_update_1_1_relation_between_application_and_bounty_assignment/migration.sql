4573fdfb078d5eea11f2803882c0b7039c2a8088
Camila Martinez <camilamarb98@gmail.com>
2024-11-14 13:34:50 +0100

    H2-14: Cleanup migrations

--- prisma/migrations/20240916163907_update_1_1_relation_between_application_and_bounty_assignment/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[applicationId]` on the table `BountyAssignment` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "BountyAssignment_applicationId_key" ON "BountyAssignment"("applicationId");
