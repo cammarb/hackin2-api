880a812b5c23d2aac0ece9fd093b18b2fab19cf1
Camila Martinez <camilamarb98@gmail.com>
2024-11-19 09:05:16 +0100

    H2-17: Cleanup and refactor

--- src/__tests__/unit/auth.middleware.test.ts
+++ src/__tests__/unit/auth.middleware.test.ts
@@ -7,33 +7,35 @@ import {
   JWTExpiredError,
   UnauthorizedError
 } from '../../error/apiError'
-
-const testUser: User = {
-  id: '1',
-  firstName: 'John',
-  lastName: 'Doe',
-  username: 'john.doe',
-  email: 'john.doe@email.com',
-  password: 'hashedPassword',
-  role: Role.ENTERPRISE,
-  mfa: true,
-  confirmed: true,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stripeAccountId: 'testStripeAccountId'
-}
-
-let tokens = {
-  accessToken: '',
-  refreshToken: ''
-}
-
-let expiredToken = ''
+import type { IncomingHttpHeaders } from 'node:http'
 
 describe('verifyJWT middleware function', () => {
-  let req: Request | any
-  let res: Response | any
+  let req: Partial<Request>
+  let res: Partial<Response>
   let next: NextFunction
+
+  const testUser: User = {
+    id: '1',
+    firstName: 'John',
+    lastName: 'Doe',
+    username: 'john.doe',
+    email: 'john.doe@email.com',
+    password: 'hashedPassword',
+    role: Role.ENTERPRISE,
+    mfa: true,
+    confirmed: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    stripeAccountId: 'testStripeAccountId'
+  }
+
+  let tokens = {
+    accessToken: '',
+    refreshToken: ''
+  }
+
+  let expiredToken = ''
+
   beforeAll(async () => {
     tokens = await generateTokens(testUser)
     expiredToken = await generateToken(testUser, '0s')
@@ -42,7 +44,8 @@ describe('verifyJWT middleware function', () => {
   beforeEach(async () => {
     req = {
       headers: {
-        authorization: `Bearer ${tokens.accessToken}`
+        authorization:
+          `Bearer ${tokens.accessToken}` as IncomingHttpHeaders['authorization']
       }
     }
     res = {
@@ -53,38 +56,38 @@ describe('verifyJWT middleware function', () => {
   })
 
   test('Should verify the jwt token and call the next() funcition', async () => {
-    await verifyJWT(req, res, next)
+    await verifyJWT(req as Request, res as Response, next)
+
     expect(next).toHaveBeenCalled()
     expect(req.username).toBe('john.doe')
     expect(req.role).toBe('ENTERPRISE')
   })
 
   test('Should throw error when jwt is missing in headers', async () => {
-    req.headers = {}
-    try {
-      await verifyJWT(req, res, next)
-    } catch (error) {
-      expect(next).toHaveBeenCalledWith(
-        new UnauthorizedError('Missing Authorization Headers')
-      )
+    req.headers = {
+      authorization: undefined
     }
+
+    await verifyJWT(req as Request, res as Response, next)
+
+    expect(next).toHaveBeenCalledWith(
+      new UnauthorizedError('Missing Authorization Headers')
+    )
   })
 
   test('Should throw `JWTExpiredError` when jwt is expired', async () => {
-    req.headers.authorization = `Bearer ${expiredToken}`
-    try {
-      await verifyJWT(req, res, next)
-    } catch (error) {
-      expect(next).toHaveBeenCalledWith(new JWTExpiredError('jwt expired'))
-    }
+    req.headers = { authorization: `Bearer ${expiredToken}` }
+
+    await verifyJWT(req as Request, res as Response, next)
+
+    expect(next).toHaveBeenCalledWith(new JWTExpiredError('jwt expired'))
   })
 
   test('Should throw `InvalidJWTError` when jwt is invalid', async () => {
-    req.headers.authorization = `Bearer invalid${expiredToken}`
-    try {
-      await verifyJWT(req, res, next)
-    } catch (error) {
-      expect(next).toHaveBeenCalledWith(new InvalidJWTError('invalid token'))
-    }
+    req.headers = { authorization: `Bearer invalid${expiredToken}` }
+
+    await verifyJWT(req as Request, res as Response, next)
+
+    expect(next).toHaveBeenCalledWith(new InvalidJWTError('invalid token'))
   })
 })
