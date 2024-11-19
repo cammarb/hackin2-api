49d72e66feea87dffce3180b7d409409c1271861
Camila Martinez <camilamarb98@gmail.com>
2024-11-19 07:50:40 +0100

    H2-17: Add seed data to jest global setup

--- src/__tests__/jest-global-setup.ts
+++ src/__tests__/jest-global-setup.ts
@@ -1,5 +1,6 @@
 // jest-global-setup.ts
 
+import { generateTestcontainerData } from './testcontainers/generateTestcontainerData'
 import {
   startPostgresContainer,
   startRedisContainer
@@ -8,4 +9,5 @@ import {
 export default async () => {
   const postgresContainer = await startPostgresContainer()
   const redisContainer = await startRedisContainer()
+  const generateData = await generateTestcontainerData()
 }
